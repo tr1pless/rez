491489fd5a7ada6b90b2f022e5c4b49a4acb063a
kadmy95@gmail.com <kadmy95@gmail.com>
2022-06-29 23:07:32 +0300

    new form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,109 +1,91 @@
 import React, { useState } from "react";
 import styles from "./form.module.css";
+import { useForm } from "react-hook-form";
 
 export const Form = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data, errors);
 
-  const [nameError, setNameError] = useState(false);
-  const [emailError, setEmailError] = useState(false);
-  const [textError, setTextError] = useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // if (name.length == 0 ?? email.length == 0 ?? text.length == 0) {
-    //   if (name.length == 0) {
-    // setNameError(true);
-    // console.log(nameError);
-    //   } else {
-    // setNameError(false);
-    //   }
-    //   if (email.length == 0) {
-    // setEmailError(true);
-    // console.log(emailError);
-    //   } else {
-    // setEmailError(false);
-    //   }
-    //   if (message.length == 0) {
-    // setTextError(true);
-    // console.log(textError);
-    //   } else {
-    // setTextError(false);
-    //   }
-    // }
-    // e.preventDefault();
-    // console.log(email);
-    // console.log(name);
-    // console.log(message);
-    // console.log(emailError);
-    // console.log(nameError);
-    // console.log(textError);
-    if (name !== "") {
-      setNameError(false);
-    } else {
-      setNameError(true);
-    }
-  };
+  const [name, setName] = useState('')
+  const [lName, setLName] = useState('')
+  const [email, setEmail] = useState('')
+  const [number, setNumber] = useState('')
+  const [message, setMessage] = useState('')
 
   return (
-    <>
-      <div className={styles.form__wrp}>
-        <h1 className={styles.form__title}>
-          Use this form to send me a message.
+    <div className={styles.form__wrp}>
+      <h1 className={styles.form__title}>
+            Use this form to send me a message.
         </h1>
-        <form className={styles.form} action="submit">
-          {emailError
-            ? true
-            : <p className={styles.error}>Enter your email.</p> || null}
-          <label className={styles.label} htmlFor="email">
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+    <label className={styles.label} htmlFor="name">
+              First name.
+            </label>
+      <input
+      onChange={(e) => setName(e.target.value)}
+      value={name}
+      id="name"
+        type="text"
+        placeholder="First name"
+        {...register("First name", { required: true, maxLength: 80 })}
+      />
+        <label className={styles.label} htmlFor="lName">
+              Last name.
+            </label>
+      <input
+      onChange={(e) => setLName(e.target.value)}
+      value={lName}
+      id="lName"
+        type="text"
+        placeholder="Last name"
+        {...register("Last name", { required: true, maxLength: 100 })}
+      />
+       <label className={styles.label} htmlFor="email">
             Email.
-          </label>
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            className={styles.email}
-            value={email}
-            id="email"
-            type="email"
-          />
-          {nameError
-            ? true
-            : <p className={styles.error}>Enter your name.</p> || null}
-          <label className={styles.label} htmlFor="name">
-            Name.
-          </label>
-          <input
-            onChange={(e) => setName(e.target.value)}
-            className={styles.name}
-            id="name"
-            type="text"
-            value={name}
-          />
-          {textError
-            ? true
-            : <p className={styles.error}>Enter your message.</p> || null}
-          <label className={styles.label} htmlFor="text">
-            Message.
-          </label>
-          <textarea
-            onChange={(e) => setMessage(e.target.value)}
-            value={message}
-            className={styles.text}
-            id="text"
-          />
-          <div className={styles.buttons__wrp}>
-            <button
-              onClick={(e) => {
-                handleSubmit(e);
-              }}
-              className={styles.form__button}
-            >
-              Submit
-            </button>
-            <button className={styles.form__button}>Clear</button>
-          </div>
-        </form>
+           </label>
+      <input
+      onChange={(e) => setEmail(e.target.value)}
+      value={email}
+      className={styles.email}
+      id='email'
+        type="text"
+        placeholder="Email"
+        {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
+      />
+    <label className={styles.label} htmlFor="number">
+              Phone number.
+            </label>
+      <input
+      value={number}
+      id="number"
+      onChange={(e) => setNumber(e.target.value)}
+        type="number"
+        placeholder="Phone number"
+        {...register("Phone number", {  maxLength: 12 })}
+      />
+      <textarea
+      value={message}
+      className={styles.text}
+      id="message"
+      onChange={(e) => setMessage(e.target.value)}
+      {...register("Text", {required: true})} />
+
+      <div className={styles.buttons__wrp}>
+        <button
+          onClick={(e) => {
+            handleSubmit(e);
+          }}
+          className={styles.form__button}
+        >
+          Submit
+        </button>
+        <button className={styles.form__button}>Clear</button>
       </div>
-    </>
+    </form>
+    </div>
   );
 };
