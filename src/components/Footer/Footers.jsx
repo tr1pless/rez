4ea3fa0488bde549726616e2b0e452ae2fd208b6
kadmy95@gmail.com <kadmy95@gmail.com>
2023-04-07 13:58:46 +0300

    footer fixed

--- src/components/Footer/Footers.jsx
+++ src/components/Footer/Footers.jsx
@@ -6,7 +6,7 @@ import styles from './footer.module.css'
 export const Footer = () => {
   return (
     <>
-      <footer className={styles.footer} id='Social'>
+      <footer className={styles.footer__container} id='Social'>
         <hr className={styles.hr} />
         <div className={styles.hr__img__wrp}>
           <Link
