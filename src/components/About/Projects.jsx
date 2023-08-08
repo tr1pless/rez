6afe4ec14afb9d4347607ea1a8a7d2d55f7ad7ef
kadmy95@gmail.com <kadmy95@gmail.com>
2023-08-08 15:41:00 +0300

    projects

--- src/components/About/Projects.jsx
+++ src/components/About/Projects.jsx
@@ -7,29 +7,33 @@ export const Projects = () => {
   const projects = [...date.projects]
   let i = 0
   return (
-    <div className={s.projects}>
+    <div className={s.projectsNav}>
+      {/* <div className={s.projectsNav}> */}
       {projects.map((item) => (
-        <div className={s.projectItem} key={i++}>
-          <h2>{item.title}</h2>
-
-          <img
-            className={s.projectImage}
-            src={item.img}
-            width={'300px'}
-            height={'150px'}
-            alt={item.title}
-          ></img>
-          <p>{item.description}</p>
-          <p>Stack:{item.stack}</p>
-          <a
-            className={s.projectLink}
-            target='_blank'
-            href={item.url !== '' ? item.url : '#'}
-          >
-            {item.title}
-          </a>
-        </div>
+        <button className={s.projectsNavBtn}>{item.title}</button>
       ))}
+      {/* </div> */}
+      {/* {projects.map((item) => ( */}
+      {/* <div className={s.projectItem} key={i++}> */}
+      {/* <h2>{item.title}</h2> */}
+      {/* <img */}
+      {/* className={s.projectImage} */}
+      {/* src={item.img} */}
+      {/* width={'300px'} */}
+      {/* height={'150px'} */}
+      {/* alt={item.title} */}
+      {/* ></img> */}
+      {/* <p>{item.description}</p> */}
+      {/* <p>Stack:{item.stack}</p> */}
+      {/* <a */}
+      {/* className={s.projectLink} */}
+      {/* target='_blank' */}
+      {/* href={item.url !== '' ? item.url : '#'} */}
+      {/* > */}
+      {/* {item.title} */}
+      {/* </a> */}
+      {/* </div> */}
+      {/* ))} */}
     </div>
   )
 }
